491aadaf53f56a9d10b5c1e6944d33116dc27226
Karoline Dias Barreto <karolinedbarreto@gmail.com>
2021-05-08 13:23:40 -0300

    watch

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,48 @@
 <template>
   <div id="app">
+    <input type="text" v-model="text">
+    <span>{{ text }}</span>
+    <span>{{ apiResponse }}</span>
   </div>
 </template>
 
 <script>
   export default {
     name: 'App',
+    data(){
+      return {
+        apiResponse: 'Start',
+        text: ''
+      }
+    },
+    methods:{
+      simulateApi(response){
+        return new Promise((res) => {
+          setTimeout(() => {
+            this.apiResponse = response;
+          }, 1000)
+        })
+      },
+      async fetchApi(value){
+        await this.simulateApi(value);
+      },
+      callApi(newValue, oldValue){
+        console.log(newValue, oldValue);
+        this.fetchApi(Math.random());
+      }
+    },
+    watch:{
+      // text(){
+      //   this.fetchApi(Math.random());
+      // },
+      // text: 'callApi',
+      text:{
+        immediate: true, //inicia com um valor
+        handler(){
+          this.fetchApi(Math.random());
+        }
+      }
+    }
   }
 
 </script>

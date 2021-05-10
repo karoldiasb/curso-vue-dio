e7191a4b56f041a1836eb0ba0cb46ce4002bfd04
Karoline Dias Barreto <karolinedbarreto@gmail.com>
2021-05-10 10:56:09 -0300

    hooks do ciclo de vida dos componentes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <template>
   <div id="app">
+    <Title v-if="shouldAppear"></Title>
+    <!-- <button @click="count++">Click</button> -->
+    <button @click="shouldAppear = false">Destroy title</button>
+    <!-- {{ count }} -->
   </div>
 </template>
 
 <script>
+  import Title from './Title.vue';
+
   export default {
     name: 'App',
+    components: {
+      Title
+    },
+    data(){
+      return {
+        hello: 'world',
+        count: 0,
+        shouldAppear: true
+      }
+    },
+    beforeCreate(){
+      console.log('Before create app')
+      console.log(this.hello)
+    },
+    created(){
+      console.log('Created app')
+      console.log(this.hello)
+    },
+    beforeMount(){
+      console.log('Before mount app')
+      console.log(this.$el)
+    },
+    mounted(){
+      console.log('Mounted app')
+      console.log(this.$el)
+    },
+    beforeUpdate(){
+      console.log('Before update app')
+      console.log(this.count)
+    },
+    updated(){
+      console.log('Updated app')
+      console.log(this.count)
+    }
   }
 
 </script>
---
+++ src/Title.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>Vue.js</h1>
+</template>
+
+<script>
+export default {
+    name: 'Title',
+    data(){
+        return {}
+    },
+    beforeCreate(){
+      console.log('Before create title')
+      console.log(this.hello)
+    },
+    created(){
+      console.log('Created title')
+      console.log(this.hello)
+    },
+    beforeMount(){
+      console.log('Before mount title')
+      console.log(this.$el)
+    },
+    mounted(){
+      console.log('Mounted title')
+      console.log(this.$el)
+    },
+    beforeDestroy(){
+      console.log('Before destroy title')
+    },
+    destroyed(){
+      console.log('Destroyed title')
+    }
+}
+</script>

92ea0aa8fb07a3200f7c9149e87985ccc584831b
Karoline Dias Barreto <karolinedbarreto@gmail.com>
2021-05-13 22:02:58 -0300

    Propriedades e diretivas, passagem de dados com propriedades e diretiva v-for

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,87 @@
 <template>
   <div id="app">
+    <!-- passando valores pro componente filho -->
+    <Title titulo="Vuejs" :tipo="1" class="red" attr1="attr1"/> <!-- adiciona a class red no root do Title, ou seja, na div pai -->
+    <Title titulo="Titulo 2" subtitulo="Basico"/>
+
+    <ul>
+      <li v-for="(item, i) in todos" :key="i">
+        {{item}}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="(valor, chave, index) in todosObject" :key="index">
+        {{ chave }} : {{ valor }} - {{ index }}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="(char, indexString) in todoString" :key="indexString">
+        {{ char }} - {{ indexString }}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="number in 10" :key="number">
+        {{ number }}
+      </li>
+    </ul>
+    <ul>
+      <template v-if="listTodos.length > 0"> <!--serve pra encapsular-->
+        <li v-for="(todo, k) in filteredTodos" :key="k">
+          {{ todo }}
+        </li>
+      </template>
+      <li v-else>No item</li>
+    </ul>
+
+    <!-- component separado -->
+    <ul>
+      <ListItem
+        v-for="(item, indexItem) in listItem"
+        :key="indexItem"
+        v-bind="item"
+      />
+    </ul>
+
   </div>
 </template>
 
 <script>
+  import Title from './Title.vue';
+  import ListItem from './listItem.vue';
+
   export default {
     name: 'App',
+    components: {
+      Title,
+      ListItem
+    },
+    data(){
+      return {
+        todos: [
+          'item 1',
+          'item 2',
+          'item 3',
+        ],
+        todosObject:{
+          item1: 'Item 1 object',
+          item2: 'Item 2 object',
+          item3: 'Item 3 object',
+        },
+        todoString: 'Hello world Vue.js',
+        listTodos: [
+        ],
+        listItem:[
+          { label: 'item 1', value: 1},
+          { label: 'hello', value: 'world'},
+          { label: 'harry', value: 'potter'},
+        ]
+      }
+    },
+    computed:{
+      filteredTodos(){
+        return this.listTodos.filter(todo => !todo.isCompleted)
+      }
+    }
   }
 
 </script>
---
+++ src/listItem.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <li> {{label}} - {{value}} </li>
+
+</template>
+
+<script>
+export default {
+    name: 'List-Item',
+    props: {
+        label: String,
+        value: [String, Number]
+    }
+}
+</script>
